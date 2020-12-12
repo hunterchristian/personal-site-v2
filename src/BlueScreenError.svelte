e6dbeb8bfe8c749332d452796e54d9158bce74d0
Hunter Hodnett <hchodnett@gmail.com>
2020-12-12 13:06:22 -0600

    Add random chance to display error screen

---
+++ src/BlueScreenError.svelte
@@ -0,0 +1,19 @@
+<style>
+  .blue-screen-error {
+    background-image: url('/images/0x000000D1.webp');
+    background-size: cover;
+    z-index: 6;
+    color: white;
+    background-color: blue;
+    position: fixed;
+    top: 0;
+    right: 0;
+    left: 0;
+    bottom: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
+
+<div class="blue-screen-error" />
